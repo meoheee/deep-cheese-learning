431e9388031845326fd4e6e9be295759ee2d0317
강대희 <45695476+meoheee@users.noreply.github.com>
2023-01-13 22:18:14 +0900

    asdf

--- defective/app.py
+++ defective/app.py
@@ -4,16 +4,27 @@ import os
 app = Flask(__name__)
 
 
-imgDir = 'templates/images'
+imgDir = '/Users/meoheee/Documents/GitHub/deep-cheese-learning/defective/templates/images'
 imgLst = os.listdir(imgDir)
-imgLst = [i for i in imgLst if i[-4]==".png" or i[-4]==".jpg"]
+imgLst = [i for i in imgLst if i[-4:]==".png" or i[-4:]==".jpg"]
 errorCnt = len(imgLst)
+print(errorCnt)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     try:
         testlist = [i for i in range(errorCnt)]
         a = render_template('mainpage_2.html', myList=testlist)
+    except:
+        a = "NONO"
+    return a
+
+
+@app.route('/mapView')
+def mapView():
+    try:
+        a = render_template('index.html', a=35.441524, b=126.469718)
     except:
         a = "NONO"
     return a
